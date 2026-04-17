956ff493ad092f492991ecba6252f23b86c2cfaa
Luca Casarin <luca.casarin.003@gmail.com>
2026-04-17 12:25:23 +0200

    Delete Ode-Numerical-Solvers directory

--- Ode-Numerical-Solvers/Euler-explicit/euler_explicit.m
+++
@@ -1,9 +0,0 @@
-function [t,y] = euler_explicit(t0,y0,t_end,h,fcn)
-
-n = fix((t_end-t0)/h)+1;
-t = linspace(t0,t0+(n-1)*h,n)';
-y = zeros(n,1);
-y(1) = y0;
-for i = 2:n
-  y(i) = y(i-1) + h*feval(fcn,t(i-1),y(i-1));
-end
